b6004b3d84039224bfbd7b7b2b3bc9bfcc94a0e0
Mohamad <mohamadslmi0019@gmail.com>
2025-07-03 02:15:13 +0330

    feat: find dynamic ip on server

--- PofferServer/PofferServer/pofferserver.cpp
+++ PofferServer/PofferServer/pofferserver.cpp
@@ -2,17 +2,48 @@
 #include "clienthandler.h"
 #include <QThread>
 #include <QDebug>
+#include <QNetworkInterface>
 
 PofferServer::PofferServer(QObject *parent) : QTcpServer(parent)
 {
 }
 
+// void PofferServer::startServer(quint16 port)
+// {
+//     if (this->listen(QHostAddress::Any, port)) {
+//         qDebug() << "Server started on port" << port;
+//     } else {
+//         qDebug() << "Server could not start!";
+//     }
+// }
+
 void PofferServer::startServer(quint16 port)
 {
+    // listen to all ip
     if (this->listen(QHostAddress::Any, port)) {
-        qDebug() << "Server started on port" << port;
+        qDebug() << "Server started successfully.";
+
+        //valid ip
+        QString localIp = "127.0.0.1";
+        const QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+        for (const QHostAddress &address : ipAddressesList) {
+            // ip that is ipv4
+            if (address != QHostAddress::LocalHost && address.toIPv4Address()) {
+                localIp = address.toString();
+                break;
+            }
+        }
+
+        // print
+        qDebug() << "========================================";
+        qDebug() << "Server is listening on:";
+        qDebug() << "IP:  " << localIp;
+        qDebug() << "Port:" << port;
+        qDebug() << "========================================";
+        qDebug() << "Waiting for clients to connect...";
+
     } else {
-        qDebug() << "Server could not start!";
+        qCritical() << "Server could not start! Error:" << this->errorString();
     }
 }
 
